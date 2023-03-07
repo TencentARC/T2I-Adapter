a366e7ace45e2e51c3f56c7861262883bce5a68f
ToTheBeginning <wuyanze123@gmail.com>
2023-03-07 16:34:06 +0800

    add pytorch lightning training code
    Former-commit-id: 4634bf5bce66505892ed72d79d525ea9749ae4e4 [formerly 48b759f8b48f046faee110f682575d2689631b53] [formerly aa8c00c9deb293bbaffa021a12de642372c2ac58 [formerly aa8c00c9deb293bbaffa021a12de642372c2ac58 [formerly a6223039a683c277777be0a1ce4edf82f3abb3c3]]]
    Former-commit-id: 1470af952666f28c305d99213a0e1dd653941617 [formerly 696c8cada9b8ee3f86ac87557489ee96c687dbc9]
    Former-commit-id: 73331e607acefc43ba87c801c9c716483751c267
    Former-commit-id: 856b5504ee52f726192026f2b3ea885b6f711405

--- ldm/data/utils.py
+++ ldm/data/utils.py
@@ -25,6 +25,26 @@ class AddCannyFreezeThreshold(object):
         return sample
 
 
+class AddCannyRandomThreshold(object):
+
+    def __init__(self, low_threshold=100, high_threshold=200, shift_range=50):
+        self.low_threshold = low_threshold
+        self.high_threshold = high_threshold
+        self.threshold_prng = np.random.RandomState()
+        self.shift_range = shift_range
+
+    def __call__(self, sample):
+        # sample['jpg'] is PIL image
+        x = sample['jpg']
+        img = cv2.cvtColor(np.array(x), cv2.COLOR_RGB2BGR)
+        low_threshold = self.low_threshold + self.threshold_prng.randint(-self.shift_range, self.shift_range)
+        high_threshold = self.high_threshold + self.threshold_prng.randint(-self.shift_range, self.shift_range)
+        canny = cv2.Canny(img, low_threshold, high_threshold)[..., None]
+        sample['canny'] = img2tensor(canny, bgr2rgb=True, float32=True) / 255.
+        sample['jpg'] = to_tensor(x)
+        return sample
+
+
 class AddStyle(object):
 
     def __init__(self, version):
